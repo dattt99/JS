0be2455f71b925d0cd92f41e8e8be1960bc00792
dattt99 <86552642+dattt99@users.noreply.github.com>
2022-04-01 16:00:05 +0700

    ok comit

--- Server/server.js
+++ Server/server.js
@@ -2,22 +2,46 @@ import  express from "express"
 import products from "./data/Products.js";
 import dotenv from "dotenv"
 import connectDatabase from './config/MongoDb.js';
-
+import ImportData from "./DataImport.js";
+import swaggerUI from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
+import productRoute from "./Routes/ProductRoutes.js";
 
 dotenv.config();
 connectDatabase(); 
 const app = express()
 
-//Load product from server
-app.get("/api/products", (req,res) => {
-    res.json(products)
-});
+const options = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "Library API",
+        version: "1.0.0",
+        description: "A simple Express Library API",
+        termsOfService: "http://example.com/terms/",
+        contact: {
+          name: "API Support",
+          url: "http://www.exmaple.com/support",
+          email: "support@example.com",
+        },
+      },
+  
+      servers: [
+        {
+          url: "http://localhost:5000",
+          description: "My API Documentation",
+        },
+      ],
+    },
+    apis: ["./Routes/*.js"],
+  };
+  
+  const specs = swaggerJsDoc(options);
+  app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
-//Single project from server
-app.get("/api/products/:id", (req,res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
+//API
+app.use("/api/import", ImportData)
+app.use("/api/products", productRoute)
 
 app.get("/", (req,res) => {
     res.send("Api is Runing.... ")
